028df130b01d8f361b1306f72a0af31fc0ac9f21
pipex <falvarez@xerenity.co>
2024-02-21 15:40:24 -0500

    Prettify root files in /pages as well as scss files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
-import '@styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '@styles/globals.scss';
+import type { AppProps } from 'next/app';
 // Next.js allows you to import CSS directly in .js files.
 // It handles optimization and all the necessary Webpack configuration to make this work.
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import { SSRProvider } from 'react-bootstrap'
-import { ProgressBar } from '@components/ProgressBar'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { SSRProvider } from 'react-bootstrap';
+import { ProgressBar } from '@components/ProgressBar';
 
 // You change this configuration value to false so that the Font Awesome core SVG library
 // will not try and insert <style> elements into the <head> of the page.
 // Next.js blocks this from happening anyway so you might as well not even try.
 // See https://fontawesome.com/v6/docs/web/use-with/react/use-with#next-js
-config.autoAddCss = false
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   // In server-side rendered applications, a SSRProvider must wrap the application in order
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </SSRProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
