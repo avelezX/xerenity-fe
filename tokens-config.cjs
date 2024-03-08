eb7167a7201360c8dda294cdf229a53d03cfd1d8
Felipe Alvarez <160548634+falvarex@users.noreply.github.com>
2024-03-08 14:53:32 -0500

    Design System Setup (#38)
    * Color variables, font-family replacement and more sass overrides
    
    * Remove submodules
    
    * Brand logo addition and removal of not needed favicons
    
    ---------
    
    Co-authored-by: svelezsaffon <svelezsaffon@santiagacstudio.lan>

--- tokens-config.cjs
+++ tokens-config.cjs
@@ -8,9 +8,27 @@ const FORMAT = 'scss/variables';
 const excludeJsonWrapper = ({ name }) =>
   name.replace(/^(xerenity-)|(.*)$/, '$2');
 
+// Transform token values to be easily consumed in SCSS
+const valueTransformer = ({ value, type }) => {
+  if (
+    type === 'borderWidth' ||
+    type === 'fontSizes' ||
+    type === 'borderRadius'
+  ) {
+    return `${value}px`;
+  } else if (type === 'fontFamilies') {
+    return `'${value}'`;
+  }
+  return value;
+};
+
 const config = {
   source: [SOURCE],
   transform: {
+    valueTransform: {
+      type: 'value',
+      transformer: valueTransformer,
+    },
     prefixRemoval: {
       type: 'name',
       transformer: (prop) => excludeJsonWrapper(prop),
@@ -18,7 +36,10 @@ const config = {
   },
   platforms: {
     scss: {
-      transforms: StyleDictionary.transformGroup.scss.concat(['prefixRemoval']),
+      transforms: StyleDictionary.transformGroup.scss.concat([
+        'valueTransform',
+        'prefixRemoval',
+      ]),
       buildPath: PATH,
       files: [
         {
