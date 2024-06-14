3f6fba0f1a1bf0a0a81bb52dc412f2c5a4c690c2
pipex <falvarez@xerenity.co>
2024-06-14 11:31:54 -0500

    Run prettify in missing files

--- src/components/chart/ChartContext.tsx
+++ src/components/chart/ChartContext.tsx
@@ -1,11 +1,11 @@
-import { createContext, useContext } from "react";
-import { IChartApi } from "lightweight-charts";
+import { createContext, useContext } from 'react';
+import { IChartApi } from 'lightweight-charts';
 
-export const ChartContext = createContext<IChartApi | undefined>( undefined);
+export const ChartContext = createContext<IChartApi | undefined>(undefined);
 
-export function useChartContext(){
-    const context = useContext(ChartContext);
-    return context;
+export function useChartContext() {
+  const context = useContext(ChartContext);
+  return context;
 }
 
 export default useChartContext;
--- src/types/condf.tsx
+++ src/types/condf.tsx
@@ -1,12 +1,12 @@
 export interface CopNdf {
-    days_diff_effective_expiration: number;
-    trade_count: number;
-    total_sum_notional_leg_2: number;
-    average_exchange_rate: number;
-    min_exchange_rate: number;
-    median_exchange_rate: number;
-    max_exchange_rate: number;
-    last_trade_time: string;
-    last_exchange_rate: number;
-    last_volume: number;
-};
+  days_diff_effective_expiration: number;
+  trade_count: number;
+  total_sum_notional_leg_2: number;
+  average_exchange_rate: number;
+  min_exchange_rate: number;
+  median_exchange_rate: number;
+  max_exchange_rate: number;
+  last_trade_time: string;
+  last_exchange_rate: number;
+  last_volume: number;
+}
