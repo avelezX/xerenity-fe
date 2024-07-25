a5b1e0ad015e64fc2e7f34f4935f500428cdcadb
Xerenity svelez <svelezx@santiagos-MacBook-Pro.local>
2024-07-25 17:48:39 -0600

    Improve looks of reste password page

--- src/pages/login/reset/index.tsx
+++ src/pages/login/reset/index.tsx
@@ -8,11 +8,13 @@ import {
   FormikConfig,
   prepareDataForValidation,
 } from 'formik';
-import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash, faLock } from '@fortawesome/free-solid-svg-icons';
 import Button from '@components/UI/Button';
 import Spinner from '@components/UI/Spinner';
 import { useState } from 'react';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import IconButton from '@components/UI/IconButton';
+
 import * as Yup from 'yup';
 import Alert from '@components/UI/Alert';
 import strings from '../../../strings/resetPassword.json';
@@ -23,13 +25,14 @@ const { form } = strings;
 
 const LOGO_SETTINGS = {
   url: '/assets/img/brand/logo.svg',
-  width: '200',
+  width: '400',
   alt: 'xerenity logo',
 };
 
 function ResetPasswordPage() {
   const supabase = createClientComponentClient();
   const [message, setMessage] = useState<string>('');
+  const [showPassword, setShowPassword] = useState<boolean>(true);
 
   const resetPasswordSchema = Yup.object().shape({
     password: Yup.string().min(10).required(form.required),
@@ -60,80 +63,85 @@ function ResetPasswordPage() {
   };
 
   return (
-    <div className="container-fluid h-75">
-      <div className="bg-white min-vh-100 d-flex flex-column justify-content-between">
-        <Image
-          src={LOGO_SETTINGS.url}
-          fluid
-          draggable="false"
-          width={LOGO_SETTINGS.width}
-          alt={LOGO_SETTINGS.alt}
-          className="mx-auto d-block pb-3"
-        />
+    <div className="container-fluid w-50 h-90">
+      <div className=" row min-vh-100">
+        <div className="bg-white min-vh-100 d-flex flex-column justify-content-between">
+          <Image
+            src={LOGO_SETTINGS.url}
+            fluid
+            draggable="false"
+            width={LOGO_SETTINGS.width}
+            alt={LOGO_SETTINGS.alt}
+            className="mx-auto d-block pb-3"
+          />
+          <Formik
+            initialValues={initialValues}
+            onSubmit={onSubmit}
+            validationSchema={resetPasswordSchema}
+            validate={(values) => {
+              if (values.password !== values.confirmPassword) {
+                return {
+                  confirmPassword: form.noigual,
+                };
+              }
+              return {};
+            }}
+          >
+            {({ values, handleChange, isSubmitting, handleSubmit }) => (
+              <Form
+                onSubmit={handleSubmit}
+                className="d-flex justify-content-center flex-column gap-3"
+              >
+                <Form.Group controlId="password">
+                  <InputGroup>
+                    <InputGroup.Text className="bg-white">
+                      <Icon className="text-primary" icon={faLock} fixedWidth />
+                    </InputGroup.Text>
+                    <Form.Control
+                      placeholder={form.password}
+                      type={showPassword ? 'text' : 'password'}
+                      value={values.password}
+                      onChange={handleChange}
+                    />
+                  </InputGroup>
+                  <ErrorMessage name="password">
+                    {(msg: string) => <ErrorMsg>{msg}</ErrorMsg>}
+                  </ErrorMessage>
+                </Form.Group>
+                <Form.Group controlId="confirmPassword">
+                  <InputGroup>
+                    <InputGroup.Text className="bg-white">
+                      <Icon className="text-primary" icon={faLock} fixedWidth />
+                    </InputGroup.Text>
+                    <Form.Control
+                      placeholder={form.confirma}
+                      type={showPassword ? 'text' : 'password'}
+                      value={values.confirmPassword}
+                      onChange={handleChange}
+                    />
+                  </InputGroup>
+                  <ErrorMessage name="confirmPassword">
+                    {(msg: string) => <ErrorMsg>{msg}</ErrorMsg>}
+                  </ErrorMessage>
+                </Form.Group>
+                <div className="d-flex justify-content-end gap-4">
+                  <IconButton onClick={() => setShowPassword(!showPassword)}>
+                    <Icon
+                      className="text-primary"
+                      icon={showPassword ? faEye : faEyeSlash}
+                      fixedWidth
+                    />
+                  </IconButton>
+                  <Button type="submit">
+                    {form.action}
+                    {isSubmitting && <Spinner size="sm" />}
+                  </Button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+          {message && <Alert>{message}</Alert>}
 
-        <Formik
-          initialValues={initialValues}
-          onSubmit={onSubmit}
-          validationSchema={resetPasswordSchema}
-          validate={(values) => {
-            if (values.password !== values.confirmPassword) {
-              return {
-                confirmPassword: form.noigual,
-              };
-            }
-            return {};
-          }}
-        >
-          {({ values, handleChange, isSubmitting, handleSubmit }) => (
-            <Form
-              onSubmit={handleSubmit}
-              className="w-50 d-flex justify-content-center flex-column gap-3"
-            >
-              <Form.Group controlId="password">
-                <InputGroup>
-                  <InputGroup.Text className="bg-white">
-                    <Icon className="text-primary" icon={faLock} fixedWidth />
-                  </InputGroup.Text>
-                  <Form.Control
-                    placeholder={form.password}
-                    type="password"
-                    value={values.password}
-                    onChange={handleChange}
-                  />
-                </InputGroup>
-                <ErrorMessage name="password">
-                  {(msg: string) => <ErrorMsg>{msg}</ErrorMsg>}
-                </ErrorMessage>
-              </Form.Group>
-              <Form.Group controlId="confirmPassword">
-                <InputGroup>
-                  <InputGroup.Text className="bg-white">
-                    <Icon className="text-primary" icon={faLock} fixedWidth />
-                  </InputGroup.Text>
-                  <Form.Control
-                    placeholder={form.confirma}
-                    type="password"
-                    value={values.confirmPassword}
-                    onChange={handleChange}
-                  />
-                </InputGroup>
-                <ErrorMessage name="confirmPassword">
-                  {(msg: string) => <ErrorMsg>{msg}</ErrorMsg>}
-                </ErrorMessage>
-              </Form.Group>
-              <div className="d-flex justify-content-end">
-                <Button type="submit">
-                  {form.action}
-                  {isSubmitting && <Spinner size="sm" />}
-                </Button>
-              </div>
-            </Form>
-          )}
-        </Formik>
-        <div className="w-50 d-flex justify-content-center flex-column gap-3">
-          <Alert>{message}</Alert>
-        </div>
-        <div className="py-5">
           <PoweredBy />
         </div>
       </div>
