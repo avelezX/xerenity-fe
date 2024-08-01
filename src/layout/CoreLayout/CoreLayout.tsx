349559fbd1801a1ed2ff37b574489941817d836c
Felipe Alvarez <160548634+falvarex@users.noreply.github.com>
2024-08-01 12:12:10 -0500

    Mobile Warning (#127)

--- src/layout/CoreLayout/CoreLayout.tsx
+++ src/layout/CoreLayout/CoreLayout.tsx
@@ -1,12 +1,22 @@
-import React, { PropsWithChildren, useCallback, useEffect } from 'react';
+import React, {
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
+import { isMobile } from 'react-device-detect';
 import Head from 'next/head';
 import Sidebar from '@layout/CoreLayout/Sidebar/Sidebar';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { deleteCookie, getCookie } from 'cookies-next';
 import { useRouter } from 'next/router';
+import MobileWarning from './MobileWarning';
+
+const LOGIN_PATH = '/login';
 
 export default function CoreLayout({ children }: PropsWithChildren) {
   const router = useRouter();
+  const [mobileUI, setMobileUI] = useState(false);
   const supabase = createClientComponentClient();
 
   const getRedirect = () => {
@@ -19,12 +29,26 @@ export default function CoreLayout({ children }: PropsWithChildren) {
     return '/';
   };
 
+  const logout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      router.push(LOGIN_PATH);
+    } else {
+      router.push(LOGIN_PATH);
+    }
+  };
+
   const checkUserLogedIn = useCallback(async () => {
     const {
       data: { session },
     } = await supabase.auth.getSession();
+
     if (!session) {
       router.push(getRedirect());
+      setMobileUI(false);
+    } else if (session && isMobile) {
+      // If user is logged and it's using a mobile device notify us
+      setMobileUI(true);
     }
   }, [supabase, router]);
 
@@ -39,12 +63,16 @@ export default function CoreLayout({ children }: PropsWithChildren) {
         <meta name="description" content="Xerenity Financial tools" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="layout-wrapper">
-        <Sidebar />
-        <div className="layout-content">
-          <div>{children}</div>
+      {mobileUI ? (
+        <MobileWarning onLogout={logout} />
+      ) : (
+        <div className="layout-wrapper">
+          <Sidebar />
+          <div className="layout-content">
+            <div>{children}</div>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
