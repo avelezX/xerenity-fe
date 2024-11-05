1b3e873aef2f83bc1012b124f54e1abcc04f42b2
Xerenity svelez <svelezsaffon@gmail.com>
2024-11-05 17:35:32 -0600

    More chnages

--- src/components/Table/columnDefinition/loans/loanDebt/columns.ts
+++ src/components/Table/columnDefinition/loans/loanDebt/columns.ts
@@ -13,48 +13,23 @@ const LoanDebtListColumns = [
     sortable: true,
   },
   {
-    name: 'Irr',
+    name: 'WAC IRR',
     selector: (row: LoanData) => currencyFormat(row.average_irr),
+    sortable: true,
+  },
+  {
+    name: 'WACC IBR (IRR)',
+    selector: (row: LoanData) => currencyFormat(row.average_irr_ibr),
+    sortable: true,
+  },
+  {
+    name: 'Spread',
+    selector: () => 'Falta',
     sortable: true,
   },
   {
     name: 'Tenor',
     selector: (row: LoanData) => currencyFormat(row.average_tenor),
-    sortable: true,
-  },
-  {
-    name: 'Irr IBR',
-    selector: (row: LoanData) => currencyFormat(row.average_irr_ibr),
-    sortable: true,
-  },
-  {
-    name: 'Intereses',
-    selector: (row: LoanData) => currencyFormat(row.accrued_interest),
-    sortable: true,
-  },
-  {
-    name: 'Duracion',
-    selector: (row: LoanData) => currencyFormat(row.average_duration),
-    sortable: true,
-  },
-  {
-    name: 'IRR Fija',
-    selector: (row: LoanData) => currencyFormat(row.average_irr_fija),
-    sortable: true,
-  },
-  {
-    name: 'Valor Total IBR',
-    selector: (row: LoanData) => currencyFormat(row.total_value_ibr),
-    sortable: true,
-  },
-  {
-    name: 'Valor Fija',
-    selector: (row: LoanData) => currencyFormat(row.total_value_fija),
-    sortable: true,
-  },
-  {
-    name: 'Valor Total',
-    selector: (row: LoanData) => currencyFormat(row.total_value),
     sortable: true,
   },
 ];
