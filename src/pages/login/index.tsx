6f9af8c9b8004e0e6adc21437dbf474c4ac6dc5e
svelezsaffon <svelezsaffon@gmail.com>
2025-07-14 22:16:43 -0500

    Remove sign up form (#147)
    * Remove sign up form
    
    * Fix lint issues

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,28 +1,11 @@
 'use client';
 
-import { useState } from 'react';
 import { NextPage } from 'next';
 import { Image } from 'react-bootstrap';
-import { Tab, Tabs, TabItemType } from '@components/UI/Tabs';
 import LoginForm from './_LoginForm';
-import SingUpForm from './_SignUpForm';
 import LoginChart from './_LoginChart';
 import PoweredBy from './_PoweredBy';
 import LoginContainer from './_LoginContainer.styled';
-
-// fyi: Order of this array afects rendering
-const TAB_ITEMS: TabItemType[] = [
-  {
-    name: 'Iniciar Sesion',
-    property: 'login',
-    active: true,
-  },
-  {
-    name: 'Registrarme',
-    property: 'register',
-    active: false,
-  },
-];
 
 const LOGO_SETTINGS = {
   url: '/assets/img/brand/logo.svg',
@@ -30,54 +13,28 @@ const LOGO_SETTINGS = {
   alt: 'xerenity logo',
 };
 
-const LoginPage: NextPage = () => {
-  const [formTabs, setTabs] = useState(TAB_ITEMS);
-
-  const handleTabChange = (prop: string) => {
-    setTabs((prevState) =>
-      prevState.map((tab) => ({
-        ...tab,
-        active: tab.property === prop,
-      }))
-    );
-  };
-
-  return (
-    <LoginContainer>
-      <aside className="form-wrapper">
-        <div className="container">
-          <Image
-            src={LOGO_SETTINGS.url}
-            fluid
-            draggable="false"
-            width={LOGO_SETTINGS.width}
-            alt={LOGO_SETTINGS.alt}
-            className="mx-auto d-block pb-3"
-          />
-          <nav className="login-tabs">
-            <Tabs>
-              {formTabs.map(({ active, name, property }) => (
-                <Tab
-                  active={active}
-                  key={name}
-                  onClick={() => handleTabChange(property)}
-                >
-                  {name}
-                </Tab>
-              ))}
-            </Tabs>
-          </nav>
-          <div className="login-forms">
-            {formTabs[0].active ? <LoginForm /> : <SingUpForm />}
-          </div>
+const LoginPage: NextPage = () => (
+  <LoginContainer>
+    <aside className="form-wrapper">
+      <div className="container">
+        <Image
+          src={LOGO_SETTINGS.url}
+          fluid
+          draggable="false"
+          width={LOGO_SETTINGS.width}
+          alt={LOGO_SETTINGS.alt}
+          className="mx-auto d-block pb-3"
+        />
+        <div className="login-forms">
+          <LoginForm />
         </div>
-        <PoweredBy />
-      </aside>
-      <figure className="chart-wrapper">
-        <LoginChart />
-      </figure>
-    </LoginContainer>
-  );
-};
+      </div>
+      <PoweredBy />
+    </aside>
+    <figure className="chart-wrapper">
+      <LoginChart />
+    </figure>
+  </LoginContainer>
+);
 
 export default LoginPage;
