64c2e62966e281100f7d7e9e9964f44df76904d7
svelezsaman <svelezsaffon@gmail.com>
2025-07-14 22:10:25 -0500

    Remove sign up form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,9 +3,8 @@
 import { useState } from 'react';
 import { NextPage } from 'next';
 import { Image } from 'react-bootstrap';
-import { Tab, Tabs, TabItemType } from '@components/UI/Tabs';
+import { TabItemType } from '@components/UI/Tabs';
 import LoginForm from './_LoginForm';
-import SingUpForm from './_SignUpForm';
 import LoginChart from './_LoginChart';
 import PoweredBy from './_PoweredBy';
 import LoginContainer from './_LoginContainer.styled';
@@ -16,11 +15,6 @@ const TAB_ITEMS: TabItemType[] = [
     name: 'Iniciar Sesion',
     property: 'login',
     active: true,
-  },
-  {
-    name: 'Registrarme',
-    property: 'register',
-    active: false,
   },
 ];
 
@@ -54,21 +48,8 @@ const LoginPage: NextPage = () => {
             alt={LOGO_SETTINGS.alt}
             className="mx-auto d-block pb-3"
           />
-          <nav className="login-tabs">
-            <Tabs>
-              {formTabs.map(({ active, name, property }) => (
-                <Tab
-                  active={active}
-                  key={name}
-                  onClick={() => handleTabChange(property)}
-                >
-                  {name}
-                </Tab>
-              ))}
-            </Tabs>
-          </nav>
           <div className="login-forms">
-            {formTabs[0].active ? <LoginForm /> : <SingUpForm />}
+            <LoginForm />
           </div>
         </div>
         <PoweredBy />
